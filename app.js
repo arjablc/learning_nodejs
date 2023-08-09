b4e0dbc494bf9a3a15fc7564827a1ffdc7d94127
arjablc <arjablc@gmail.com>
2023-08-09 20:59:31 +0545

    name and password submitted store in db

--- app.js
+++ app.js
@@ -4,9 +4,33 @@ const { user } = require("./model/index.js");
 
 require("./model/index.js");
 
+//Using this to parse the req and res to json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //setting up ejs and telling nodejs to use ejs
 app.set("view engine", "ejs");
+
+
+app.get("/", (req, res)=>{
+	res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+	res.render("login_page.ejs");
+});
+app.post("/login", (req, res) => {
+	const { userName, password } = req.body;
+	//this will create a new entity in the 
+	//user table with the name and the password columns
+	//filled with the data provided into the request 
+	//coming from the login_page.ejs file(frontend)
+	user.create({
+		name: userName,
+		password: password,
+	});
+
+});
 
 app.get("/variable", (req, res) => {
 	res.render("variable.ejs", { variable: "Hello World" });
